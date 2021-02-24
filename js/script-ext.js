a2fb0ff6004bc8a8ccfad7ac1d8d6d5c329d0809
fabianafk <70484512+fabianafk@users.noreply.github.com>
2021-02-24 14:36:42 -0300

    troca de fundo js (fica com o fundo branco)

---
+++ js/script-ext.js
@@ -0,0 +1,5 @@
+let signUpBtn = document.querySelector('.home-main.menu.btn.sign.up')
+
+signUpBtn.addEventListener('click', () =>{
+    document.body.style.backgroundImage = "url('img/fundo-signup.png')";
+})    
